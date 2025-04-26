5cf1994f442a00713d7247c3725a896501b3bd1b
Brandon Pardi <72064522+b-pardi@users.noreply.github.com>
2025-04-26 16:21:38 -0700

    init commit, download official llama variants from meta

---
+++ dl_llama_models.py
@@ -0,0 +1,65 @@
+import pexpect.popen_spawn
+import os
+import sys
+import subprocess
+from dotenv import load_dotenv
+
+models = {
+    '3.2': [
+        "Llama3.2-1B-Instruct",
+        "Llama3.2-3B-Instruct",
+        "Llama3.2-1B-Instruct:int4-qlora-eo8",
+        "Llama3.2-1B-Instruct:int4-spinquant-eo8",
+        "Llama3.2-3B-Instruct:int4-qlora-eo8",
+        "Llama3.2-3B-Instruct:int4-spinquant-eo8",
+        "Llama3.2-1B",
+        "Llama3.2-3B"
+    ],
+
+    '3.1': [
+        "Llama3.1-8B-Instruct",
+        "Llama3.1-8B"
+    ],
+
+    '3.0': [
+        "Llama-3-8B-Instruct",
+        "Llama-3-8B"
+    ]
+}
+
+load_dotenv("llama_links.env")
+url_dict = {
+    '3.2': os.getenv("llama3_2_id_url"),
+    '3.1': os.getenv("llama3_1_id_url"),
+    '3.0': os.getenv("llama3_0_id_url")
+}
+
+for (ver, ids) in models.items():
+    for model_id in ids:
+        id_url = url_dict[ver]
+        cmd = f"llama model download --source meta --model-id  {model_id}"
+        verify_cmd = verify_cmd = ["llama", "model", "verify-download", "--model-id", model_id]
+        
+        try:
+            child = pexpect.popen_spawn.PopenSpawn(cmd, encoding='utf-8')
+            child.logfile = sys.stdout
+            child.expect("Please provide the signed URL for model", timeout=5) 
+            child.sendline(id_url)
+            child.expect("[Optionally] To run MD5 checksums,", timeout=600)
+            child.expect(pexpect.EOF)
+            print(f"Finished download for: {id_url}")
+
+        except pexpect.exceptions.TIMEOUT:
+            print(f"\nTimeout occurred for {model_id}")
+
+        except Exception as e:
+            print(f"\nError for {model_id}: {e}")
+
+        subprocess.run(verify_cmd, check=True)
+
+# another sanity check for hashes
+for (ver, ids) in models.items():
+    for model_id in ids:
+        print('\n***',model_id)
+        verify_cmd = verify_cmd = ["llama", "model", "verify-download", "--model-id", model_id]
+        subprocess.run(verify_cmd, check=True)
---
+++ notes.md
@@ -0,0 +1,28 @@
+https://apxml.com/posts/ultimate-system-requirements-llama-3-models
+https://www.llama.com/llama-downloads/?utm_source=llama-home-hero&utm_medium=llama-referral&utm_campaign=llama-utm&utm_offering=llama-downloads&utm_product=llama
+
+## 3.2
+3b
+3b-instruct-q8_0
+3b-instruct-fp16
+3b-instruct-q4_1
+3b-instruct-q5_1
+3b-instruct-q5_K_M
+3b-instruct-q6_K
+
+
+## 3.1
+8b-instruct-q8_0
+8b-instruct-q6_K
+8b-instruct-q4_1
+8b-instruct-q4_K_S
+8b-instruct-q2_K
+
+
+## 3
+8b-instruct-q4_1
+8b-instruct-q4_K_S
+8b-instruct-q6_K
+
+other links
+https://openrouter.ai/models?max_price=0&q=llama
